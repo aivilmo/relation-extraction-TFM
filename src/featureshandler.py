6550a9e5392c966a3d491d2021ff1e5112a8e247
aivilmo <aitanagirlpotter@gmail.com>
2022-03-08 21:27:27 +0100

    fix: CHANGE statics methods to class methods

--- src/featureshandler.py
+++ src/featureshandler.py
@@ -43,47 +43,45 @@ class FeaturesHandler:
         columns = []
 
         if "single_word_emb" in self._features:
-            FeaturesHandler._feat_single_word_emb(df)
+            self._feat_single_word_emb(df)
             columns += ["word"]
         if "tf_idf" in self._features:
-            FeaturesHandler._feat_tf_idf(df, test=test)
+            self._feat_tf_idf(df, test=test)
             columns += ["word"]
         if "with_entities" in self._features:
-            FeaturesHandler._feat_with_tags(df, test)
+            self._feat_with_tags(df, test)
             columns += ["tag1"] + ["tag2"]
         if "word_emb" in self._features:
-            FeaturesHandler._feat_word_emb(df)
+            self._feat_word_emb(df)
             columns += ["word1"] + ["word2"]
         if "sent_emb" in self._features:
-            FeaturesHandler._feat_sent_emb(df)
+            self._feat_sent_emb(df)
             columns += ["sentence"]
         if "bag_of_words" in self._features:
-            FeaturesHandler._feat_bag_of_words(df, test=test)
+            self._feat_bag_of_words(df, test=test)
             columns += ["word"]
         if "chars" in self._features:
-            FeaturesHandler._feat_chars(df, test=test)
+            self._feat_chars(df, test=test)
             columns += ["word"]
         if "bert" in self._features or "distil_bert" in self._features:
-            FeaturesHandler._feat_bert(df)
+            self._feat_bert(df)
             columns += ["word"]
 
         features: np.ndarray = FeaturesHandler._combine_features(df, columns)
         print("Features matrix succesfully generated, with data shape:", features.shape)
         return np.nan_to_num(features)
 
-    @staticmethod
-    def _feat_with_tags(df: pd.DataFrame, test: bool = False) -> None:
+    def _feat_with_tags(self, df: pd.DataFrame, test: bool = False) -> None:
         if not test:
             print("Fitting word labels")
-            df["tag1"] = FeaturesHandler.instance()._le.fit_transform(df.tag1.values)
-            df["tag2"] = FeaturesHandler.instance()._le.fit_transform(df.tag2.values)
+            df["tag1"] = self._le.fit_transform(df.tag1.values)
+            df["tag2"] = self._le.fit_transform(df.tag2.values)
             return
 
-        df["tag1"] = FeaturesHandler.instance()._le.transform(df.tag1.values)
-        df["tag2"] = FeaturesHandler.instance()._le.transform(df.tag2.values)
+        df["tag1"] = self._le.transform(df.tag1.values)
+        df["tag2"] = self._le.transform(df.tag2.values)
 
-    @staticmethod
-    def _feat_word_emb(df: pd.DataFrame) -> None:
+    def _feat_word_emb(self, df: pd.DataFrame) -> None:
         if not Embedding.trained():
             tokens = Embedding.prepare_text_to_train(df)
             WordEmbedding.instance().load_model(
@@ -98,8 +96,7 @@ class FeaturesHandler:
             lambda x: WordEmbedding.instance().words_to_vector(x.split())
         )
 
-    @staticmethod
-    def _feat_sent_emb(df: pd.DataFrame) -> None:
+    def _feat_sent_emb(self, df: pd.DataFrame) -> None:
         if not Embedding.trained():
             tokens = Embedding.prepare_text_to_train(df)
             WordEmbedding.instance().load_model(
@@ -111,26 +108,19 @@ class FeaturesHandler:
             lambda x: WordEmbedding.instance().words_to_vector(x.split())
         )
 
-    @staticmethod
     def _feat_bag_of_words(
-        df: pd.DataFrame, column: str = "word", test: bool = False
+        self, df: pd.DataFrame, column: str = "word", test: bool = False
     ) -> None:
         if not test:
             print("Fitting words to bag of words")
-            FeaturesHandler.instance()._cv.fit(df[column].unique().tolist())
-            print(
-                "Vocab size: ", len(FeaturesHandler.instance()._cv.vocabulary_.keys())
-            )
+            self._cv.fit(df[column].unique().tolist())
+            print("Vocab size: ", len(self._cv.vocabulary_.keys()))
 
         df[column] = df[column].apply(
-            lambda x: FeaturesHandler.instance()
-            ._cv.transform([x])
-            .toarray()
-            .reshape(-1)
+            lambda x: self._cv.transform([x]).toarray().reshape(-1)
         )
 
-    @staticmethod
-    def _feat_single_word_emb(df: pd.DataFrame) -> None:
+    def _feat_single_word_emb(self, df: pd.DataFrame) -> None:
         if not Embedding.trained():
             WordEmbedding.instance().load_model(
                 "..\\dataset\\word-embeddings_fasttext\\EMEA+scielo-es_skipgram_w=10_dim=100_minfreq=1_neg=10_lr=1e-4.bin"
@@ -138,45 +128,35 @@ class FeaturesHandler:
 
         df["word"] = df.word.apply(lambda x: WordEmbedding.instance().word_vector(x))
 
-    @staticmethod
     def _feat_tf_idf(
-        df: pd.DataFrame, column: str = "word", test: bool = False
+        self, df: pd.DataFrame, column: str = "word", test: bool = False
     ) -> None:
         if not test:
             print("Fitting words to tf idf")
-            FeaturesHandler.instance()._tf.fit(df[column].unique().tolist())
-            print(
-                "Vocab size: ", len(FeaturesHandler.instance()._tf.vocabulary_.keys())
-            )
+            self._tf.fit(df[column].unique().tolist())
+            print("Vocab size: ", len(self._tf.vocabulary_.keys()))
 
         df[column] = df[column].apply(
-            lambda x: FeaturesHandler.instance()
-            ._tf.transform([x])
-            .toarray()
-            .reshape(-1)
+            lambda x: self._tf.transform([x]).toarray().reshape(-1)
         )
 
-    @staticmethod
-    def _feat_chars(df: pd.DataFrame, test: bool = False) -> None:
+    def _feat_chars(self, df: pd.DataFrame, test: bool = False) -> None:
         from keras.preprocessing.sequence import pad_sequences
 
         if not test:
-            FeaturesHandler.instance()._tk.fit_on_texts(df.word)
-            vocab_size = len(FeaturesHandler.instance()._tk.word_index) + 1
+            self._tk.fit_on_texts(df.word)
+            vocab_size = len(self._tk.word_index) + 1
             print(f"Vocab size: {vocab_size}")
 
-        df["word"] = df.word.apply(
-            lambda x: FeaturesHandler.instance()._tk.texts_to_sequences([x])[0]
-        )
+        df["word"] = df.word.apply(lambda x: self._tk.texts_to_sequences([x])[0])
         df["word"] = df.word.apply(lambda x: pad_sequences([x], maxlen=15)[0])
         print(f"Matriz features for emebdding: {df.word.shape}")
 
-    @staticmethod
-    def _feat_bert(df: pd.DataFrame) -> None:
+    def _feat_bert(self, df: pd.DataFrame) -> None:
         if not Embedding.trained():
-            if "bert" in FeaturesHandler.instance()._features:
+            if "bert" in self._features:
                 BERTEmbedding.instance().build_BERT_model()
-            elif "distil_bert" in FeaturesHandler.instance()._features:
+            elif "distil_bert" in self._features:
                 BERTEmbedding.instance().build_distilBERT_model()
 
         df["word"] = df.word.apply(lambda x: BERTEmbedding.instance().word_vector(x))
