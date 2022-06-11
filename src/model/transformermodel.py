c3fd785e7a45cbf8b468fbfe8711fd60b22d7c35
aivilmo <aitanagirlpotter@gmail.com>
2022-06-11 15:16:26 +0200

    fix: ADD train with transformers

--- src/model/transformermodel.py
+++ src/model/transformermodel.py
@@ -11,6 +11,7 @@ class TransformerModel(AbstractModel):
     _instance = None
 
     _epochs = 1
+    _save_path = "data\\models\\transformer_"
 
     @staticmethod
     def instance():
@@ -19,16 +20,21 @@ class TransformerModel(AbstractModel):
         return TransformerModel._instance
 
     def __init__(self, train, test) -> None:
+        from main import Main
+
         super().__init__()
         if TransformerModel._instance is not None:
             raise Exception
 
-        self._labels = list(train["tag"].unique())
+        self._labels = list(train.tag.unique())
         self._dataset_train = datasets.Dataset.from_dict(train, split="train")
         self._dataset_test = datasets.Dataset.from_dict(test, split="test")
         self._optimizer = None
         self._learning_rate_scheduler = None
         self._training_steps = None
+        self._model_name = Main.instance().features()[0]
+        self._model = None
+        self._is_trained = True
 
         TransformerModel._instance = self
 
@@ -41,25 +47,27 @@ class TransformerModel(AbstractModel):
         model,
     ) -> None:
         self.build()
-        self.train()
+        if not self._is_trained:
+            self.train()
         self.evaluate()
-        # self.export_results()
+        self.export_results()
 
     def build(self) -> None:
         from transformers import get_scheduler
         from torch.optim import AdamW
-        from main import Main
 
-        feat = Main.instance().features()[0]
+        self._logger.info(f"Building model {self._model_name}")
+        tr_instance = TransformerEmbedding.instance()
 
-        self._logger.info(f"Building model {feat}")
+        self.load_model()
+        if self._model == None:
+            if not tr_instance.trained():
+                tr_instance.build_transformer_to_finetuning(
+                    self._model_name, len(self._labels)
+                )
+            self._model = tr_instance._encoder_layer
+            self._is_trained = False
 
-        if not TransformerEmbedding.trained():
-            TransformerEmbedding.instance().build_transformer_to_finetuning(
-                feat, len(self._labels)
-            )
-
-        self._model = TransformerEmbedding.instance()._encoder_layer
         self._X = self.get_data_loader(self._dataset_train)
         self._y = self.get_data_loader(self._dataset_test)
         self._optimizer = AdamW(self._model.parameters(), lr=5e-5)
@@ -72,11 +80,12 @@ class TransformerModel(AbstractModel):
         )
 
         self._logger.info(
-            f"Model {feat} has built with optimizer {self._optimizer}, training steps {self._training_steps}"
+            f"Model {self._model_name} has built with optimizer {self._optimizer}, training steps {self._training_steps}"
         )
 
     def train(self) -> None:
         from tqdm.auto import tqdm
+        from torch import save
 
         self._logger.info(f"Training transformer model...")
         progress_bar = tqdm(range(self._training_steps))
@@ -94,20 +103,18 @@ class TransformerModel(AbstractModel):
                 self._optimizer.zero_grad()
                 progress_bar.update(1)
 
-            self._logger.info(f"Model has successfully trained")
+        self._logger.info(f"Model has successfully trained")
+        save(self._model, self._save_path + self._model_name)
 
     def evaluate(self) -> None:
         from tqdm.auto import tqdm
-        from torch import no_grad, argmax
+        from torch import no_grad, argmax, empty, cat
 
         self._logger.info(f"Evaluating transformer model...")
         progress_bar = tqdm(range(len(self._y)))
 
-        accuracy = datasets.load_metric("accuracy")
-        precision = datasets.load_metric("precision")
-        recall = datasets.load_metric("recall")
-        f1score = datasets.load_metric("f1")
-
+        yhat: empty = empty([0])
+        y: empty = empty([0])
         self._model.eval()
         for batch in self._y:
             batch = {k: v for k, v in batch.items()}
@@ -116,19 +123,13 @@ class TransformerModel(AbstractModel):
 
             logits = outputs.logits
             predictions = argmax(logits, dim=-1)
-
-            accuracy.add_batch(predictions=predictions, references=batch["labels"])
-            precision.add_batch(predictions=predictions, references=batch["labels"])
-            recall.add_batch(predictions=predictions, references=batch["labels"])
-            f1score.add_batch(predictions=predictions, references=batch["labels"])
-
+            yhat = cat((yhat, predictions), axis=0)
+            y = cat((y, batch["labels"]), axis=0)
             progress_bar.update(1)
 
-        self._logger.info(f"Evaluation: ")
-        self._logger.info(accuracy.compute())
-        self._logger.info(precision.compute(average="macro"))
-        self._logger.info(recall.compute(average="macro"))
-        self._logger.info(f1score.compute(average="macro"))
+        yhat = cat([yhat[:0], yhat[1:]]).numpy()
+        y = cat([y[:0], y[1:]]).numpy()
+        super().evaluate(yhat, y)
 
     def tokenize(self, dataset):
         labels = datasets.ClassLabel(num_classes=len(self._labels), names=self._labels)
@@ -146,10 +147,20 @@ class TransformerModel(AbstractModel):
         tokenized_datasets.set_format("torch")
         return tokenized_datasets
 
-    def get_data_loader(self, dataset, batch_size=24):
+    def get_data_loader(self, dataset, batch_size=20):
         from torch.utils.data import DataLoader
 
         self._logger.info("Loading dataloader from dataset {dataset}")
         tokenized_datasets = self.tokenize(dataset)
-        tokenized_datasets = tokenized_datasets.shuffle(seed=42).select(range(10))
+        tokenized_datasets = tokenized_datasets.shuffle(seed=42)  # .select(range(48))
         return DataLoader(tokenized_datasets, batch_size=batch_size)
+
+    def load_model(self) -> None:
+        from torch import load
+
+        try:
+            path = self._save_path + self._model_name
+            self._model = load(path)
+            self._logger.info(f"Model loaded successfully from path {path}")
+        except:
+            self._logger.info("Model has not found, we must to train it")
